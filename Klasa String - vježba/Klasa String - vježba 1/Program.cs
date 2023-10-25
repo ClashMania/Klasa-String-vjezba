208521b44a9e968fdee86fac6e436c1b8c99127e
JustGabi <113007885+ClashMania@users.noreply.github.com>
2023-10-25 08:31:00 +0200

    Update Program.cs

--- Klasa String - vježba/Klasa String - vježba 1/Program.cs
+++ Klasa String - vježba/Klasa String - vježba 1/Program.cs
@@ -12,7 +12,6 @@ namespace Klasa_String___vježba_1
         {
             string s1 = "Hrvatska ima more";
             string s2 = "Italija je prvak svijeta";
-
             Console.WriteLine(s1.Substring(0, 8) + s2.Substring(7, s2.Length - 7));
 
             Console.ReadKey();
